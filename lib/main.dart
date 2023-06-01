d244e49802fbd50e2569a2b9107e38f7409c38d4
evjulius <julius.kangarlow@outlook.com>
2023-06-01 15:04:04 +0800

    :arrow_up: Upgrade dependencies
    Remove firebase from app

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,8 @@
-import 'dart:io';
-
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
 import 'screens/home.dart';
 
 void main() async {
-  if (!kIsWeb && !Platform.isWindows) {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-  }
   runApp(const MyApp());
 }
 
